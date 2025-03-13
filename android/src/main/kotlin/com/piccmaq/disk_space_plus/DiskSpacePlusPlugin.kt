3f0ef815ed3cf81d7703facbd413a94f4e42ae7e
retendo <retendo@users.noreply.github.com>
2025-03-13 11:41:45 +0100

    Update DiskSpacePlusPlugin.kt

--- android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
+++ android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
@@ -1,32 +1,29 @@
 package com.piccmaq.disk_space_plus
 
+import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
 
 class DiskSpacePlusPlugin: FlutterPlugin {
 
-  companion object {
     private var channel: MethodChannel? = null
-    private var handler: MethodHandlerImpl = MethodHandlerImpl()
-
-    @JvmStatic
-    fun registerWith(registrar: PluginRegistry.Registrar) {
-      registerChannel(registrar.messenger())
-    }
+    private lateinit var handler: MethodHandlerImpl
+    private lateinit var context: Context
 
     private fun registerChannel(messenger: BinaryMessenger) {
-      channel = MethodChannel(messenger, "disk_space_plus")
-      channel!!.setMethodCallHandler(handler)
+        channel = MethodChannel(messenger, "disk_space_plus")
+        channel!!.setMethodCallHandler(handler)
     }
-  }
 
-  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    registerChannel(binding.binaryMessenger)
-  }
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        context = binding.applicationContext
+        handler = MethodHandlerImpl(context)
+        registerChannel(binding.binaryMessenger)
+    }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel = null
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        channel = null
+    }
 }
